51321a5d8a61b324e0e16937316eda2a9e62a46f
Pavel Neplyuev <33624570+higelak@users.noreply.github.com>
2023-11-04 23:40:42 +0300

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,39 +1,31 @@
 workspace "incoil"
-	architecture "x64"
+architecture "x64"
 
-	configurations 
-	{ 
-		"Debug", 
-		"Release" 
-	}
-	
-	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+configurations { "Debug", "Release" }
 
-	project "incoil"
-		location "."
-		kind "ConsoleApp"
-		language "C++"
-		cppdialect "C++14"
-		staticruntime "off"
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
-		targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
-		objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+project "incoil"
+	location "."
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++14"
+	staticruntime "off"
 
-		files 
-		{ 
-			"src/**.h", 
-			"src/**.cpp" 
-		}
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files { "src/**.h", "src/**.cpp" }
 
 	filter "system:windows"
 	systemversion "latest"
 			
 	filter "configurations:Debug"
-		defines { "DEBUG" }
-		runtime "Debug"
-		symbols "On"
+	defines { "DEBUG" }
+	runtime "Debug"
+	symbols "On"
 
 	filter "configurations:Release"
-		defines { "NDEBUG" }
-		runtime "Release"
-		optimize "On"
+	defines { "NDEBUG" }
+	runtime "Release"
+	optimize "On"
