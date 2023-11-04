4dd90c518815e7512f43a12418fa0d8183b3aa82
Pavel Neplyuev <ranet107@gmail.com>
2023-11-04 23:18:26 +0300

    Using premake

---
+++ premake5.lua
@@ -0,0 +1,39 @@
+workspace "incoil"
+	architecture "x64"
+
+	configurations 
+	{ 
+		"Debug", 
+		"Release" 
+	}
+	
+	outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+	project "incoil"
+		location "."
+		kind "ConsoleApp"
+		language "C++"
+		cppdialect "C++14"
+		staticruntime "off"
+
+		targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+		objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+		files 
+		{ 
+			"src/**.h", 
+			"src/**.cpp" 
+		}
+
+	filter "system:windows"
+	systemversion "latest"
+			
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		runtime "Debug"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		runtime "Release"
+		optimize "On"
